8b9427642ce999bc7e802381b386e556b2eae4ac
CesarSantana <CesarSantana@students.olin.edu>
2014-11-20 12:05:53 -0500

    initial commit

---
+++ cointoss.m
@@ -0,0 +1,17 @@
+function res = cointoss(t,W)
+    P=W(1:2);
+    V=W(3:4); 
+    Theta=W(5:6);
+    m=0.00567;
+    dtdt=omega
+    function res = omega()
+        res = [0;0];
+    end
+    dpdt=V
+    dvdt=acceleration
+    function res = acceleration()
+        g=9.8;
+        res =[0;-g];
+    end
+    res=[dpdt;dvdt;dtdt];
+end
