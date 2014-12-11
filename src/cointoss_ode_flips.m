fdce8525a23e5e3822d4e4c655a7e371075664fd
Isaac Getto <isaac.getto@gmail.com>
2014-12-11 14:22:18 -0500

    add data

---
+++ src/cointoss_ode_flips.m
@@ -0,0 +1,55 @@
+function cointoss_ode_flips
+    [T, M] = cointoss_ode_precise;
+
+    X = M(:, 1);
+    Y = M(:, 2);
+    Vx = M(:, 3);
+    Vy = M(:, 4);
+    Theta = M(:, 5);
+    Omega = M(:, 6);
+
+    clf;
+    hold on;
+
+    tMin = min(T);
+    tMax = max(T);
+    yMin = min(Y);
+    yMax = max(Y);
+
+    theta = 0;
+    prevT = 0;
+    color = 'r';
+    for i = 1:size(Theta)
+        if Theta(i) - theta >= 180
+            drawRect(prevT, T(i), yMin, yMax, color);
+
+            if  color == 'r'
+                color = 'b';
+            else
+                color = 'r';
+            end
+            theta = theta + 180;
+            prevT = T(i);
+        end
+    end
+    drawRect(prevT, T(end), yMin, yMax, color);
+
+    axis([tMin, tMax, yMin, yMax]);
+
+    plot(T, Y, 'LineWidth', 4, 'Color', 'k');
+
+    xlabel('t (s)');
+    ylabel('y (m)');
+    title('position vs. time');
+
+    % weird legend
+    plot([-10, -10], [-10, -10], 'LineWidth', 4, 'Color', 'r');
+    plot([-10, -10], [-10, -10], 'LineWidth', 4, 'Color', 'b');
+    legend('position', 'heads', 'tails');
+
+    function drawRect(x1, x2, y1, y2, color)
+        w = x2 - x1;
+        h = y2 - y1; 
+        rectangle('Position', [x1, y1, w, h], 'FaceColor', color, 'EdgeColor', color);
+    end
+end
